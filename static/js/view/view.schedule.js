f8856257e3d799fa640fb65d3276edb05d9ef7df
Joseph Thomas <joey6978@gmail.com>
2017-05-03 19:25:41 -0700

    Changing View, just a little more to go

---
+++ static/js/view/view.schedule.js
@@ -0,0 +1,39 @@
+var Schedule = new Vue({
+  el: '#calendar',
+  data: {
+    days: [
+      [{time: 0, sections: []}],
+      [],
+      [],
+      [],
+      []
+    ]
+  }
+})
+
+
+Schedule.addSection = function (section) {
+  var time = Mu.Model.timeMap[section.schedule]
+
+  :NextDay
+  for (var i = 0; i < 5; i++) {
+    if (!time[i]) {
+      continue
+    }
+
+    // If there's a interesect, add to it.
+    for (var g = Schedule.days[i]; g--;) {
+      var nT = Schedule.days[i].time
+      if (nT & time[i]) {
+        Schedule.days[i].time |= time[i]
+        Schedule.days[i].sections.push(section)
+        continue NextDay
+      }
+    }
+    // No interesect, make a new group.
+    Schedule.days[i].push({
+      time: time[i],
+      sections: [section]
+    }) 
+  }
+}
