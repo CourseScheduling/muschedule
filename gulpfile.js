088406eea0f31f62ecc06b640db66672ed92f225
Joseph Thomas <joey6978@gmail.com>
2017-06-08 19:39:19 -0700

    Commiting for design branch

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,30 @@
 var gulp = require('gulp')
 var concat = require('gulp-concat')
 var uglify = require('gulp-uglify')
+var async = require('async')
 
-var files = [
-  "./static/js/vue.js",
-  "./static/js/mu.main.js",
-  "./static/js/mu.model.js",
-  "./static/js/mu.view.js",
-  "./static/js/view/view.control.js",
-  "./static/js/view/view.generate.js",
-  "./static/js/view/view.schedule.js",
-  "./static/js/util.js"
-]
+gulp.task('build', cb => {
 
-gulp.task('build', function () {
-  return gulp.src(files)
+  async.parallel([
+    cb => {
+      // This is for the css.
+      
+    },
+    cb => {
+      // This if for the js.
+
+    }
+  ],
+  function end(err,results) {
+    if (err) {
+      console.err('Error', err)
+      return
+    }
+    
+    cb(results)
+  })
+
+  gulp.src(files)
   .pipe(uglify())
   .pipe(concat('bundle.js'))
   .pipe(gulp.dest('./static/js'))
